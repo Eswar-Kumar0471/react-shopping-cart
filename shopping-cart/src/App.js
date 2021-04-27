1b7cd1c2d0042a598b0ba0b66b53a9183fc71bbc
unknown <eswarkumar.boina@gmail.com>
2021-04-27 11:06:40 +0530

    first commit

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href='/'>React Shopping</a>
+      </header>
+      <main>
+        Product List
+        </main>
+        <footer>
+          All right is reserved 2021
+        </footer>
+    </div>
+  );
+}
+
+export default App;
